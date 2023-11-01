4464a38e2e3168fb3316d945b1a8f1248848771f
HOME <HOME@LYKOV>
2023-11-01 12:17:01 +0200

    Добавьте файлы проекта.

---
+++ PazzlePazzle/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace PazzlePazzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button8 = new Button();
+            button7 = new Button();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            button9 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button8);
+            panel1.Controls.Add(button7);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(153, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(270, 270);
+            panel1.TabIndex = 0;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(90, 180);
+            button8.Name = "button8";
+            button8.Size = new Size(90, 90);
+            button8.TabIndex = 7;
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button1_Click_1;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(0, 180);
+            button7.Name = "button7";
+            button7.Size = new Size(90, 90);
+            button7.TabIndex = 6;
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button1_Click_1;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(180, 90);
+            button6.Name = "button6";
+            button6.Size = new Size(90, 90);
+            button6.TabIndex = 5;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button1_Click_1;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(90, 90);
+            button5.Name = "button5";
+            button5.Size = new Size(90, 90);
+            button5.TabIndex = 4;
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button1_Click_1;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(0, 90);
+            button4.Name = "button4";
+            button4.Size = new Size(90, 90);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button1_Click_1;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(180, 0);
+            button3.Name = "button3";
+            button3.Size = new Size(90, 90);
+            button3.TabIndex = 2;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(90, 0);
+            button2.Name = "button2";
+            button2.Size = new Size(90, 90);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button1_Click_1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(0, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(90, 90);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(153, 297);
+            button9.Name = "button9";
+            button9.Size = new Size(258, 46);
+            button9.TabIndex = 1;
+            button9.Text = "Start Game";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(567, 373);
+            Controls.Add(button9);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button8;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Button button9;
+    }
+}
